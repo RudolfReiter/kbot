8ca773753eb1406d68bbcb947e06e7578572ca63
RudolfReiter <reiter11@gmx.at>
2024-10-02 12:34:57 +0200

    adding number of parts per order to output

--- src/planner_cpp/src/orderOptimizer.cpp
+++ src/planner_cpp/src/orderOptimizer.cpp
@@ -61,7 +61,7 @@ void OrderOptimizer::next_order_callback(const kbot_interfaces::msg::NextOrder::
 
 void OrderOptimizer::get_order_(Position &current_pos, Order &order)
 {
-  unordered_map<string, vector<string>> partid2products;
+  unordered_map<string, vector<int>> partid2productids;
   set<string> all_parts;
 
   tuple_pos start{current_pos.x, current_pos.y, "current position"};
@@ -72,7 +72,7 @@ void OrderOptimizer::get_order_(Position &current_pos, Order &order)
     for (auto &part : products_[product_nr].part_count)
     {
       all_parts.insert(get<0>(part));
-      partid2products[get<0>(part)].push_back(products_[product_nr].product_name);
+      partid2productids[get<0>(part)].push_back(product_nr);
     }
   }
   vector<string> all_parts_vec(all_parts.begin(), all_parts.end());
@@ -89,20 +89,25 @@ void OrderOptimizer::get_order_(Position &current_pos, Order &order)
   std::vector<string> path = result.second;
   std::stringstream ss;
 
-  RCLCPP_INFO(this->get_logger(), "Working on order %d (%s)", current_order_.order_nr, order_descr_.c_str());
   ss << "Working on order " << current_order_.order_nr << " (" << order_descr_ << ")" << endl;
+  int n_products = 0;
+  string plural_part_str = "part";
 
   for (auto &part : path)
   {
-    for (auto &product : partid2products[part])
+    for (auto &productid : partid2productids[part])
     {
-      RCLCPP_INFO(this->get_logger(), "Fetching part \'%s\' for product \'%s\' at x: %f, y: %f",
-                  part.c_str(), product.c_str(), parts_[part].pos.x, parts_[part].pos.y);
-      ss << "Fetching part \'" << part << "\' for product \'" << product
+      n_products = products_[productid].part_count[part];
+      if (n_products > 1)
+        plural_part_str = "parts";
+      else
+        plural_part_str = "part";
+
+      ss << "Fetching " << n_products << " " << plural_part_str << " \'" << part
+         << "\' for product \'" << products_[productid].product_name
          << "\' at x: " << parts_[part].pos.x << ", y: " << parts_[part].pos.y << endl;
     }
   }
-  RCLCPP_INFO(this->get_logger(), "Delivering to destination x: %f, y: %f", order.pos.x, order.pos.y);
   ss << "Delivering to destination x: " << order.pos.x << ", y: " << order.pos.y << endl;
 
   append_to_file(path_order_data_ + "/" + out_file, ss);
@@ -146,12 +151,6 @@ bool OrderOptimizer::find_order(int order_nr)
   for (auto &t : threads)
     if (t.joinable())
       t.join();
-
-  if (is_valid)
-    RCLCPP_INFO(this->get_logger(), "Order %d found. Containing %d products for delivery to position x=%f, y=%f",
-                current_order_.order_nr, current_order_.product_nrs.size(), current_order_.pos.x, current_order_.pos.y);
-  else
-    RCLCPP_INFO(this->get_logger(), "Order %d not found...", order_nr);
 
   return is_valid;
 }
